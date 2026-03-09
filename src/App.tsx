98fc42c9a91c1cbd4223057eabea402d97d15780
Devan Huapaya <huapayadevan@gmail.com>
2026-03-08 22:19:41 -0700

    add gh pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL.replace(//
+, "")}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/timer" element={<Index />} />
